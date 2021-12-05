0add1cf2ced00c7397d58c08db8210e65d3436f9
Frank Giordano <giofr01@yahoo.com>
2021-12-05 00:34:39 -0500

    optimize imports

--- src/main/java/com/commands/JobLog.java
+++ src/main/java/com/commands/JobLog.java
@@ -7,7 +7,10 @@ import zosjobs.input.GetJobParams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class JobLog {
 
