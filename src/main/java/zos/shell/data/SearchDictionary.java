c9a1b5b6c54b8e938084a60fdccb269ac5eec824
Frank Giordano <giofr01@yahoo.com>
2023-02-12 23:45:38 -0500

    make static

--- src/main/java/zos/shell/data/SearchDictionary.java
+++ src/main/java/zos/shell/data/SearchDictionary.java
@@ -2,20 +2,16 @@ package zos.shell.data;
 
 import java.util.List;
 
-public class SearchDictionary {
+public final class SearchDictionary {
 
-    private final TriePreFix dictionary;
-
-    private final String[] commands = new String[]{"browsejob", "cancel", "cat", "cd", "change", "clear",
+    private static final String[] commands = new String[]{"browsejob", "cancel", "cat", "cd", "change", "clear",
             "clearlog", "color", "connections", "count", "copy", "download", "downloadjob", "end", "files",
             "help", "history", "ls", "mvs", "purgejob", "ps", "pwd", "rm", "save", "search", "stop", "submit",
             "tailjob", "timeout", "touch", "uname", "ussh", "vi", "visited", "whoami"};
 
-    public SearchDictionary() {
-        dictionary = new TriePreFix(commands);
-    }
+    private static final TriePreFix dictionary = new TriePreFix(commands);
 
-    public List<String> search(String prefix) {
+    public static List<String> search(String prefix) {
         return dictionary.getCommands(prefix);
     }
 
