cff53038834eea8c73afbd28e6dd6e463156fef0
Frank Giordano <giofr01@yahoo.com>
2023-03-20 21:15:41 -0400

    add some debug output

--- src/main/java/zos/shell/data/SearchDictionary.java
+++ src/main/java/zos/shell/data/SearchDictionary.java
@@ -1,8 +1,14 @@
 package zos.shell.data;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import zos.shell.commands.BrowseJob;
+
 import java.util.List;
 
 public final class SearchDictionary {
+
+    private static Logger LOG = LoggerFactory.getLogger(SearchDictionary.class);
 
     private static final String[] commands = new String[]{"browsejob", "cancel", "cat", "cd", "change", "clear",
             "clearlog", "color", "connections", "count", "copy", "copys", "download", "downloadjob", "end", "files",
@@ -12,6 +18,7 @@ public final class SearchDictionary {
     private static final TriePreFix dictionary = new TriePreFix(commands);
 
     public static List<String> search(String prefix) {
+        LOG.debug("*** search ***");
         return dictionary.getCommands(prefix);
     }
 
