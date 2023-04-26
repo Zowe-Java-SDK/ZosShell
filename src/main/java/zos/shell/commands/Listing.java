9ac343f25bc8bb1b5f399c22ff8cbcb289b143d0
Frank Giordano <giofr01@yahoo.com>
2023-04-26 00:01:03 -0400

    fix inconvertible types

--- src/main/java/zos/shell/commands/Listing.java
+++ src/main/java/zos/shell/commands/Listing.java
@@ -69,7 +69,8 @@ public class Listing {
             final var index = m.indexOf("*");
             final var searchForMember = index == -1 ? m : m.substring(0, index);
             if (m.equals(searchForMember)) {
-                members = members.stream().filter(i -> i.equals(searchForMember)).collect(Collectors.toList());
+                members = members.stream()
+                        .filter(i -> i.getMember().orElse("n\\a").equals(searchForMember)).collect(Collectors.toList());
             } else {
                 members = members.stream()
                         .filter(i -> i.getMember().orElse("").startsWith(searchForMember)).collect(Collectors.toList());
