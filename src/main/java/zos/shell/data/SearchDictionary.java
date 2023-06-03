762d3a0a73743aeb80768f1bdc5d28d79b4630c5
frankgiordano <giofr01@yahoo.com>
2023-06-02 22:38:18 -0400

    added some missing commands to autofill

--- src/main/java/zos/shell/data/SearchDictionary.java
+++ src/main/java/zos/shell/data/SearchDictionary.java
@@ -6,8 +6,9 @@ public final class SearchDictionary {
 
     private static final String[] commands = new String[]{"browsejob", "cancel", "cat", "cd", "change", "clear",
             "color", "connections", "count", "copy", "copys", "download", "downloadjob", "end", "env",
-            "files", "grep", "help", "history", "ls", "mvs", "purgejob", "ps", "pwd", "rm", "save", "search", "set",
-            "stop", "submit", "tailjob", "timeout", "touch", "tso", "uname", "ussh", "vi", "visited", "whoami"};
+            "files", "grep", "help", "history", "hostname", "ls", "mkdir", "mvs", "purgejob", "ps", "pwd",
+            "rm", "save", "search", "set", "stop", "submit", "tailjob", "timeout", "touch", "tso", "uname",
+            "ussh", "vi", "visited", "whoami"};
 
     private static final TriePreFix dictionary = new TriePreFix(commands);
 
