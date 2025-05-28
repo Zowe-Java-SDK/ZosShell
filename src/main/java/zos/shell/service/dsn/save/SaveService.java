728239821c6029736093b5abd3f8be32a07da0fc
Frank Giordano <giofr01@yahoo.com>
2025-05-27 20:49:47 -0400

    remove further HLQ restrictions

--- src/main/java/zos/shell/service/dsn/save/SaveService.java
+++ src/main/java/zos/shell/service/dsn/save/SaveService.java
@@ -34,7 +34,7 @@ public class SaveService {
 
     public ResponseStatus save(final String dataset, final String target) {
         LOG.debug("*** save ***");
-        if (DsnUtil.isMember(target) && !DsnUtil.isDataset(dataset)) {
+        if (DsnUtil.isMember(target) && dataset.isBlank()) {
             return new ResponseStatus(Constants.DATASET_NOT_SPECIFIED, false);
         }
         ExecutorService pool = Executors.newFixedThreadPool(Constants.THREAD_POOL_MIN);
