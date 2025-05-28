824f48b29bb299b250792cbfc2babb3306f869b3
Frank Giordano <giofr01@yahoo.com>
2025-05-27 20:52:29 -0400

    error consistency

--- src/main/java/zos/shell/service/dsn/concat/Concat.java
+++ src/main/java/zos/shell/service/dsn/concat/Concat.java
@@ -2,6 +2,8 @@ package zos.shell.service.dsn.concat;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import zos.shell.constants.Constants;
+import zos.shell.record.DatasetMember;
 import zos.shell.response.ResponseStatus;
 import zos.shell.service.dsn.download.Download;
 import zos.shell.utility.DsnUtil;
@@ -27,13 +29,17 @@ public class Concat {
         LOG.debug("*** cat ***");
         InputStream inputStream;
         String result;
+
+        var datasetMember = DatasetMember.getDatasetAndMember(target);
         try {
             if (DsnUtil.isMember(target)) {
                 // retrieve member data
                 inputStream = download.getInputStream(String.format("%s(%s)", dataset, target));
-            } else {
-                // retrieve sequential dataset data
+            } else if (datasetMember != null || DsnUtil.isDataset(target)) {
+                // either retrieve sequential dataset data or dataset(member) data
                 inputStream = download.getInputStream(target);
+            } else {
+                return new ResponseStatus(Constants.INVALID_DATASET_AND_MEMBER_COMBINED, false);
             }
             result = FileUtil.getTextStreamData(inputStream);
             return new ResponseStatus(result != null ? result : "no data to display", true);
