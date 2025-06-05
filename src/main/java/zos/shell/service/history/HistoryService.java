7e9351a456e0d43ed7ddf9b8e05a4ccd45b0122a
Frank Giordano <giofr01@yahoo.com>
2025-06-04 23:22:29 -0400

    use var

--- src/main/java/zos/shell/service/history/HistoryService.java
+++ src/main/java/zos/shell/service/history/HistoryService.java
@@ -36,7 +36,7 @@ public class HistoryService {
             return;
         }
         
-        String command = circularLinkedList.getSize() > 1
+        var command = circularLinkedList.getSize() > 1
             ? (direction == NavigationDirection.UP ? circularLinkedList.back() : circularLinkedList.forward())
             : circularLinkedList.head.getData();
             
