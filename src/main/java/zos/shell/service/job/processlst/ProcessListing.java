06c056fb1265eed8f8353351b9b2325a7e49c92c
Frank Giordano <giofr01@yahoo.com>
2026-02-01 16:38:41 -0500

    rebase

--- src/main/java/zos/shell/service/job/processlst/ProcessListing.java
+++ src/main/java/zos/shell/service/job/processlst/ProcessListing.java
@@ -38,9 +38,6 @@ public class ProcessListing {
             final String errMsg = ResponseUtil.getResponsePhrase(e.getResponse());
             return new ResponseStatus((errMsg != null ? errMsg : e.getMessage()), false);
         }
-        jobs.sort(Comparator.comparing(Job::getJobName)
-                .thenComparing(Job::getStatus)
-                .thenComparing(Job::getJobId));
         if (jobs.isEmpty()) {
             return new ResponseStatus(Constants.NO_PROCESS_FOUND, false);
         }
