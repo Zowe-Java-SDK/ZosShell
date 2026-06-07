aa8d16666c21965299208a16ade3d57749bfd115
frankgiordano <giofr01@yahoo.com>
2026-06-06 23:57:13 -0400

    restore

--- src/main/java/zos/shell/singleton/TerminalSingleton.java
+++ src/main/java/zos/shell/singleton/TerminalSingleton.java
@@ -133,7 +133,7 @@ public final class TerminalSingleton {
 
     private void setApplicationIcon() {
         LOG.debug("*** setApplicationIcon ***");
-        URL iconUrl = ZosShell.class.getResource("/image/zosshell.png");
+        URL iconUrl = ZosShell.class.getResource("/image/zowe-icon.png");
         if (iconUrl != null) {
             ImageIcon icon = new ImageIcon(iconUrl);
             mainTerminal.getFrame().setIconImage(icon.getImage());
